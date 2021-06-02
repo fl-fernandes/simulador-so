244dadd47745e54877bcabc395b9b4ba12c2966f
FlavioLucasFer <flaviolukfer@gmail.com>
2021-06-02 11:02:04 -0300

    initial commit

---
+++ pyos.py
@@ -0,0 +1,42 @@
+import os
+import curses
+import pycfg
+from pyarch import load_binary_into_memory
+from pyarch import cpu_t
+
+class os_t:
+	def __init__ (self, cpu, memory, terminal):
+		self.cpu = cpu
+		self.memory = memory
+		self.terminal = terminal
+
+		self.terminal.enable_curses()
+
+		self.console_str = ""
+		self.terminal.console_print("this is the console, type the commands here\n")
+
+	def printk(self, msg):
+		self.terminal.kernel_print("kernel: " + msg + "\n")
+
+	def panic (self, msg):
+		self.terminal.end()
+		self.terminal.dprint("kernel panic: " + msg)
+		self.cpu.cpu_alive = False
+		#cpu.cpu_alive = False
+
+	def interrupt_keyboard (self):
+		key = self.terminal.get_key_buffer()
+
+		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
+			strchar = chr(key)
+		elif key == curses.KEY_BACKSPACE:
+			return
+		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
+			return
+
+	def handle_interrupt (self, interrupt):
+		return
+
+	def syscall (self):
+		#self.terminal.app_print(msg)
+		return
