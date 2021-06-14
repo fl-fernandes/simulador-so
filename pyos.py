05eca060930aa5b8ac22e1880d5981abb8e117d3
Mateus Barbeiro <mateusgarcia2001@gmail.com>
2021-06-14 17:16:42 -0300

    interrupção do teclado

--- pyos.py
+++ pyos.py
@@ -28,13 +28,23 @@ class os_t:
 		key = self.terminal.get_key_buffer()
 
 		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
-			strchar = chr(key)
+			self.console_str += chr(key)
+			self.terminal.console_print("\r" + self.console_str)
+
 		elif key == curses.KEY_BACKSPACE:
+			self.console_str = self.console_str[:-1]
+			self.terminal.console_print("\r" + self.console_str)
 			return
+
 		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
+			self.terminal.console_print("\n")
+			# tenta realizar acao ...
+			self.console_str = "" # apaga informacoes de input
 			return
 
 	def handle_interrupt (self, interrupt):
+		if interrupt == pycfg.INTERRUPT_KEYBOARD:
+			self.interrupt_keyboard()
 		return
 
 	def syscall (self):
